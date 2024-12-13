8c490003e08da262e257bdea74308370028d50c0
tianzx <tianzx@aliyun.com>
2024-12-13 12:56:31 +0800

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import dynamic from "next/dynamic";
 
 // 动态导入 ExcalidrawWrapper 组件，禁用 SSR
 const ExcalidrawWrapper = dynamic(
-  () => import("./ExcalidrawWrapper"),
+  () => import("./excalidrawWrapper"),
   {
     ssr: false,
     loading: () => <div>Loading editor...</div>
